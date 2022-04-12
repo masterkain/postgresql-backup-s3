a6757c54e89b455483e7010cf82c56bd87e26bb5
Claudio Poli <masterkain@gmail.com>
2022-04-12 08:47:48 +0200

    little release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker buildx create --use
+docker buildx ls
+docker buildx build \
+  --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+  --tag masterkain/postgresql-backup-s3:0.0.1 \
+  --push \
+  .
